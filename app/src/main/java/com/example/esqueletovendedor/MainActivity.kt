65005e450d5be368d634e7ca15b7ede499ebdd04
Gabo29val <ga_al5@hotmail.com>
2022-01-11 13:27:26 -0500

    una mas

--- app/src/main/java/com/example/esqueletovendedor/MainActivity.kt
+++ app/src/main/java/com/example/esqueletovendedor/MainActivity.kt
@@ -10,5 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Toast.makeText(this, "Ahhhh", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Otro cambio", Toast.LENGTH_SHORT).show()
+
     }
 }
